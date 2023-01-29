b1829d00242c8a735a9887ac11c31cc3ff8b36d2
steve <steve.jones763@gmail.com>
2023-01-29 01:17:20 +0000

    added testing configuration for unit and integration tests + jenkinsfile.

---
+++ domain/src/test/java/com/steve/core/it/CoreServiceIT.java
@@ -0,0 +1,20 @@
+package com.steve.core.it;
+
+import com.steve.core.CoreService;
+import com.steve.core.ExampleRepositoryAdapter;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.*;
+import static org.mockito.Mockito.*;
+
+public class CoreServiceIT {
+
+    @Test
+    public void shouldReturnText() {
+        ExampleRepositoryAdapter repositoryAdapter = mock(ExampleRepositoryAdapter.class);
+        CoreService underTest = new CoreService(repositoryAdapter);
+        assertThat(underTest.getData(), is("data retrieved"));
+    }
+
+}
